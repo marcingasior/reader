0b57507d289b685af5177916789354291432a0c9
marcingasior <marcin.gasior@gmail.com>
2016-02-04 00:40:43 +0000

    rhc

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ function handleRequest(request, response){
 //Create a server
 var server = http.createServer(handleRequest);
 
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
